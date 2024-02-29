8175ba62a3e5c685888cd7acfaf82ab02d214dff
Prach Chaisiri <prach.cha@mahidol.ac.th>
2024-02-29 15:04:16 +0700

    ignore test login with oauth

---
+++ ICTAcademy/test-authen.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test-authen.aspx.cs" Inherits="ICTAcademy.test_authen" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" ID="btnLogin" Text="login"  OnClick="btnLogin_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ICTAcademy/test-authen.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICTAcademy
+{
+    public partial class test_authen : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async Task btnLogin_ClickAsync()
+        {
+            string sessionData = "{ \"userId\": 123, \"username\": \"john_doe\" }";
+
+            // Send data to PHP application
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(sessionData, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("http://placement.ict.mahidol.ac.th/login/index.php", content);
+                response.EnsureSuccessStatusCode();
+                Response.Redirect("http://placement.ict.mahidol.ac.th/login/index.php");
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            btnLogin_ClickAsync();
+        }
+    }
+}
